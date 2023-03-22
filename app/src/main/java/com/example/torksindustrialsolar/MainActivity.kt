19bbeeed3bfc708ea7cc62ac0c374db66a250444
Reginald Torkornoo <regtorks1@gmail.com>
2023-03-22 16:12:54 +0000

    Menu done

--- app/src/main/java/com/example/torksindustrialsolar/MainActivity.kt
+++ app/src/main/java/com/example/torksindustrialsolar/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.torksindustrialsolar
 
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        window.statusBarColor = Color.WHITE
+
     }
 }
