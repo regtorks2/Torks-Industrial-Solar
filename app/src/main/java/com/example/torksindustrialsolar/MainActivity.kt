157a0c7d90f66b57c284b24c8a6a0a781dce5c8e
Reginald Torkornoo <regtorks1@gmail.com>
2023-03-24 13:48:34 +0000

    Starting to do body

--- app/src/main/java/com/example/torksindustrialsolar/MainActivity.kt
+++ app/src/main/java/com/example/torksindustrialsolar/MainActivity.kt
@@ -5,14 +5,23 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.viewpager.widget.ViewPager
+import com.example.torksindustrialsolar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
         supportActionBar?.hide()
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
         window.statusBarColor = Color.WHITE
+
 
     }
 }
